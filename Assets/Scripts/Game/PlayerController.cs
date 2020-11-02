37af68493db57263b906f620674e5c8df74551ed
SemyonPolyotin <catsemen@yandex.ru>
2020-11-02 15:13:32 +0300

    Added jumping logic

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody = default;
+        [SerializeField] private float _jumpForce = 300f;
+        [SerializeField] private float _jumpRaycastDistance = 0.1f;
+
+        private void Jump()
+        {
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector3.up * _jumpForce);
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position + 0.1f * Vector3.up, -Vector3.up, _jumpRaycastDistance);
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+    }
+}
