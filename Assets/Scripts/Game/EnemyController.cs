3dc4ff9b011e39f24cef8b7c71af2e254bd97424
SemyonPolyotin <catsemen@yandex.ru>
2020-11-07 15:39:39 +0300

    Added enemy with navMeshAgent

---
+++ Assets/Scripts/Game/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Game
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent _navMeshAgent = default;
+        
+        private PlayerController _player;
+
+        private void Awake()
+        {
+            _navMeshAgent.updateRotation = true;
+        }
+
+        private void Start()
+        {
+            _player = GameObject.FindObjectOfType<PlayerController>();
+        }
+
+        private void Update()
+        {
+            _navMeshAgent.SetDestination(_player.transform.position);
+        }
+    }
+}
